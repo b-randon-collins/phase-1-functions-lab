0dd71d1d52f683134b118f0d3db1dceffcd77397
BrandonC <brandonc503@yahoo.com>
2024-07-29 11:02:38 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+  return Math.abs(42 - pickupLocation);
+}
+
+
+function distanceFromHqInFeet(pickupLocation) {
+  const blocks = distanceFromHqInBlocks(pickupLocation);
+  return blocks * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+  const differenceInBlocks = Math.abs(destination - start);
+  return differenceInBlocks * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+  const distanceInFeet = distanceTravelledInFeet(start, destination);
+
+  if (distanceInFeet > 2500) {
+    return "cannot travel that far";
+  } else if (distanceInFeet <= 400) {
+    return 0;
+  } else if (distanceInFeet <= 2000) {
+    return (distanceInFeet - 400) * 0.02;
+  } else {
+    return 25;
+  }
+}
